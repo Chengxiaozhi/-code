efd6f079e4f9f6b23b3455ab3e8ec807fabf33fc
Chengxiaozhi <xiangzhichan@126.com>
2016-04-28 12:02:11 +0800

    rotateString

--- rotateString.cs
+++ rotateString.cs
@@ -1,72 +1,31 @@
 ﻿/// <summary>
 /// 题目描述
 /*
-    对于一个矩阵，请设计一个算法从左上角(mat[0][0])开始，顺时针打印矩阵元素。
-     给定int矩阵mat,以及它的维数nxm，请返回一个数组，数组中的元素为矩阵元素的顺时针输出。
-     测试样例：
-     [[1,2],[3,4]],2,2
-     返回：[1,2,4,3]
+ 	对于一个字符串，和字符串中的某一位置，请设计一个算法，将包括i位置在内的左侧部分移动到右边，将右侧部分移动到左边。
+	给定字符串A和它的长度n以及特定位置p，请返回旋转后的结果。
+	测试样例：
+	"ABCDEFGH",8,4
+	返回："FGHABCDE"
 */
 /// </summary>
-class Printer
+class StringRotation
 {
-    public int[] clockwisePrint(int[][] mat, int n, int m)
+    public string rotateString(string A, int n, int p)
     {
         // write code here
-        //动态规划算法解决，四个方向上下左右，根据当前位置规划下一步怎么走
-        int [] result = new int [n*m];
-        int currentI = 0;
-        int currentJ = 0;
-        int maxI = n-1;
-        int maxJ = m-1;
-        int minI = 0;
-        int minJ = 0;
-        int q = 0;
-        int direction = 0; //0,1,2,3分别表示下个要走的方向上下左右
-        result[0] = mat[0][0];
-        for(int i=0;i<n*m;i++)
-        {
-            switch (direction)
-            {
-                case 0://右走
-                	result[q++] = mat[currentI][currentJ++];
-                	
-                	if (currentJ == maxJ)
-                    {
-                        minI++;                    
-                		direction = 1;
-                    }                		
-                	break;
-                case 1://下走
-                	result[q++] = mat[currentI++][currentJ];
-                	if (currentI == maxI)
-                    {
-                        maxJ--;
-                		direction = 2;
-                    }                		
-                	break;
-                case 2://左走
-                	result[q++] = mat[currentI][currentJ--];
-                	if(currentJ == minJ)
-                    {
-                        maxI--;
-                        direction = 3;
-                    }                        
-                	break;
-                case 3://上走
-                	result[q++] = mat[currentI--][currentJ];
-                	if(currentI == minI)
-                    {
-                        minJ++;
-                        direction = 0;
-                    }
-                	break;
-            }
-                
-            
-        }
-        return result;
-        
-        
+        string left = A.Substring(0,p+1);
+        string right = A.Substring(p+1,n-p-1);
+        string B = "";
+        B = right + left;
+        return B;
+    }
+
+     public string rotateString1(string A, int n, int p)
+    {
+        // write code here
+        string C = A + A;
+        string B = C.Substring(p+1,n);
+        return B;
     }
 }
+
